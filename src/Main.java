f841ecc40583746e8934f16abd87e53d38e9aa31
Manoel <omegadarkxyz@gmail.com>
2025-12-03 11:37:57 -0300

    Teste no Main da aplicação do extrairmin, HardCoded

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        HeapMinimo heap = new HeapMinimo(10);
 
+        heap.inserir(5);
+        heap.inserir(2);
+        heap.inserir(8);
+        heap.inserir(1);
+        // Aqui ja se aplica a primeira reorganização para colocar o menor numero na raiz.
+
+        System.out.println("Heap antes de extrair:");
+        heap.mostrar();
+
+        int minimo = heap.extrairMinimo();
+        // E aqui quando se extrai os filhos devem ser reorganizados de acordo com o numero substituto.
+        System.out.println("Menor elemento removido: " + minimo);
+
+        System.out.println("Heap depois de extrair:");
+        heap.mostrar();
     }
 }
+
